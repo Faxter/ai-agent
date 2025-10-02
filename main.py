c5c537727631121f6bf20fb09a9b115bf73fc8d0
faxter <info@faxxter.com>
2025-10-02 17:34:38 +0200

    implement a single prompt

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from ai-agent!")
+    _ = load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print(response.text)
+    meta_data = response.usage_metadata
+    print(f"Prompt tokens: {meta_data.prompt_token_count}")
+    print(f"Response tokens: {meta_data.candidates_token_count}")
 
 
 if __name__ == "__main__":
