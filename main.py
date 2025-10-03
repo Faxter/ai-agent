badf8261967491214323435ac33c5da2b883645a
faxter <info@faxxter.com>
2025-10-03 20:09:26 +0200

    use cli argument for prompt

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
+    if len(sys.argv) != 2:
+        print("usage: uv run main.py [prompt]")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
     _ = load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        contents=messages,
     )
     print(response.text)
     usage_meta_data = response.usage_metadata
