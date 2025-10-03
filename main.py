95b859c77d8c6ac91155883ef45a56df3663d41a
faxter <info@faxxter.com>
2025-10-03 20:43:02 +0200

    refactor: extract methods

--- main.py
+++ main.py
@@ -6,18 +6,8 @@ from google.genai import types
 
 
 def main():
-    argument_parser = argparse.ArgumentParser(
-        description="Prompt the AI model with some text"
-    )
-    _ = argument_parser.add_argument("user_prompt", type=str, help="the message prompt")
-    _ = argument_parser.add_argument(
-        "--verbose", action="store_true", help="add debugging text output"
-    )
-    args = argument_parser.parse_args()
-
-    _ = load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
+    args = parse_arguments()
+    client = setup_client()
     messages = [
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
     ]
@@ -25,10 +15,31 @@ def main():
         model="gemini-2.0-flash-001",
         contents=messages,
     )
+    print_info(response, args)
+
+
+def parse_arguments():
+    argument_parser = argparse.ArgumentParser(
+        description="Prompt the AI model with some text"
+    )
+    _ = argument_parser.add_argument("user_prompt", type=str, help="the message prompt")
+    _ = argument_parser.add_argument(
+        "--verbose", action="store_true", help="add debugging text output"
+    )
+    return argument_parser.parse_args()
+
+
+def setup_client():
+    _ = load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    return genai.Client(api_key=api_key)
+
+
+def print_info(response: types.GenerateContentResponse, arguments: argparse.Namespace):
     print(response.text)
     usage_meta_data = response.usage_metadata
-    if usage_meta_data and args.verbose:
-        print(f"User prompt: {args.user_prompt}")
+    if usage_meta_data and arguments.verbose:
+        print(f"User prompt: {arguments.user_prompt}")
         print(f"Prompt tokens: {usage_meta_data.prompt_token_count}")
         print(f"Response tokens: {usage_meta_data.candidates_token_count}")
 
